02a1a220374e30dc24fd613a1779fa8281c67a60
Grindmeow <thehumaneatermeow@gmail.com>
2024-02-12 11:46:50 +0545

    push

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+import app from "./app.js";
+import { connectToDatabase } from './db/connection.js';
+const PORT = process.env.PORT || 5000;
+connectToDatabase().then(() => {
+    app.listen(PORT, () => console.log("Server open & Connected to Database🤡"));
+}).catch((error) => console.log(error));
+//# sourceMappingURL=index.js.map
